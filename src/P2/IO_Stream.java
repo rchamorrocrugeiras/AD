e6205220dadb9beca93ddfe5b1ce325952236904
rchamorrocrugeiras <rchamorrocrugeiras@danielcastelao.org>
2026-09-22 10:42:05 +0200

    Intentando arreglar errores

--- src/P2/IO_Stream.java
+++ src/P2/IO_Stream.java
@@ -2,11 +2,10 @@ package P2;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.IOException;
 
 public class IO_Stream {
     public static void copiar() throws IOException{
-        FileInputStream entrada = new FileInputStream("texto1.txt");
+        FileInputStream entrada = new FileInputStream("texto1.txt);
         FileOutputStream saida = new FileOutputStream("texto2.txt");
 
         int byteleido;
